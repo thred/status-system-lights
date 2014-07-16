3b603a474728d581b71a6bb8412bd2debbbf7e3b
Manfred Hantschel <thred@github.com>
2014-07-16 21:26:00 +0200

    added traffic light command

---
+++ src/main/java/io/github/thred/statussystemlights/TrafficLightCommand.java
@@ -0,0 +1,120 @@
+package io.github.thred.statussystemlights;
+
+import io.github.thred.statussystemlights.console.AbstractCommand;
+import io.github.thred.statussystemlights.console.ArgumentException;
+import io.github.thred.statussystemlights.console.ConsoleAdapter;
+import io.github.thred.statussystemlights.console.Process;
+
+public class TrafficLightCommand extends AbstractCommand
+{
+
+    private final SerialInterface serialInterface;
+
+    public TrafficLightCommand(ConsoleAdapter consoleAdapter, SerialInterface serialInterface)
+    {
+        super(consoleAdapter, "trafficLight", "tl");
+
+        this.serialInterface = serialInterface;
+    }
+
+    @Override
+    public String getFormat()
+    {
+        return "<ID> [-b] [r|y|g]";
+    }
+
+    @Override
+    public String getDescription()
+    {
+        return "Updates one traffic light.";
+    }
+
+    @Override
+    public int getOrdinal()
+    {
+        return 15;
+    }
+
+    @Override
+    public int execute(String commandName, Process process) throws Exception
+    {
+        Long idValue = process.args.consumeLong();
+
+        if (idValue == null)
+        {
+            throw new ArgumentException("<ID> is missing");
+        }
+
+        int id = idValue.intValue();
+
+        if ((id < 0) || (id > 3))
+        {
+            throw new ArgumentException("Invalid <ID> (0 - 3)");
+        }
+
+        boolean blink = process.args.consumeFlag("-b");
+
+        TrafficLightCommandBuilder builder = new TrafficLightCommandBuilder();
+
+        builder.redOff(id).yellowOff(id).greenOff(id);
+
+        if (process.args.consumeFlag("r"))
+        {
+            if (blink)
+            {
+                builder.redBlink(id);
+            }
+            else
+            {
+                builder.redOn(id);
+            }
+        }
+
+        if (process.args.consumeFlag("g"))
+        {
+            if (blink)
+            {
+                builder.greenBlink(id);
+            }
+            else
+            {
+                builder.greenOn(id);
+            }
+        }
+
+        if (process.args.consumeFlag("y"))
+        {
+            if (blink)
+            {
+                builder.yellowBlink(id);
+            }
+            else
+            {
+                builder.yellowOn(id);
+            }
+        }
+
+        String arg = process.args.consumeString();
+        
+        if (arg != null) {
+            throw new ArgumentException("Invalid argument: " + arg);
+        }
+        
+        process.out.println("Sending " + builder.toString());
+        
+        serialInterface.send(builder.toString());
+        serialInterface.flush();
+
+        return 0;
+    }
+
+    @Override
+    protected String getHelpDescription()
+    {
+        return "Updates one traffic light. If no color is given, the traffic light will be turned off.\n\n" //
+            + "<ID>     The id of the traffic light (0 - 3)\n" //
+            + "[-b]     Blinking\n" //
+            + "[r|y|g]  The color\n";
+    }
+
+}
---
+++ src/main/java/io/github/thred/statussystemlights/TrafficLightCommandBuilder.java
@@ -0,0 +1,109 @@
+package io.github.thred.statussystemlights;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TrafficLightCommandBuilder
+{
+
+    public static final int GREEN = 0;
+    public static final int YELLOW = 1;
+    public static final int RED = 2;
+
+    private final List<String> commands = new ArrayList<>();
+
+    public TrafficLightCommandBuilder()
+    {
+        super();
+    }
+
+    public TrafficLightCommandBuilder greenOn(int trafficLightId)
+    {
+        return on(trafficLightId, GREEN);
+    }
+
+    public TrafficLightCommandBuilder greenOff(int trafficLightId)
+    {
+        return off(trafficLightId, GREEN);
+    }
+
+    public TrafficLightCommandBuilder greenBlink(int trafficLightId)
+    {
+        return blink(trafficLightId, GREEN);
+    }
+
+    public TrafficLightCommandBuilder yellowOn(int trafficLightId)
+    {
+        return on(trafficLightId, YELLOW);
+    }
+
+    public TrafficLightCommandBuilder yellowOff(int trafficLightId)
+    {
+        return off(trafficLightId, YELLOW);
+    }
+
+    public TrafficLightCommandBuilder yellowBlink(int trafficLightId)
+    {
+        return blink(trafficLightId, YELLOW);
+    }
+
+    public TrafficLightCommandBuilder redOn(int trafficLightId)
+    {
+        return on(trafficLightId, RED);
+    }
+
+    public TrafficLightCommandBuilder redOff(int trafficLightId)
+    {
+        return off(trafficLightId, RED);
+    }
+
+    public TrafficLightCommandBuilder redBlink(int trafficLightId)
+    {
+        return blink(trafficLightId, RED);
+    }
+
+    public TrafficLightCommandBuilder on(int trafficLightId, int color)
+    {
+        return set(trafficLightId, color, 1, 0, 0);
+    }
+
+    public TrafficLightCommandBuilder off(int trafficLightId, int color)
+    {
+        return set(trafficLightId, color, 0, 0, 0);
+    }
+
+    public TrafficLightCommandBuilder blink(int trafficLightId, int color)
+    {
+        return set(trafficLightId, color, 0, 35, 70);
+    }
+
+    public TrafficLightCommandBuilder set(int trafficLightId, int color, int state, int swtch, int repeat)
+    {
+        return send(toLedId(trafficLightId, color), state, swtch, repeat);
+    }
+
+    protected int toLedId(int trafficLightId, int color)
+    {
+        return (trafficLightId * 3) + 2 + color;
+    }
+
+    protected TrafficLightCommandBuilder send(int ledId, int state, int swtch, int repeat)
+    {
+        commands.add(String.format("u %d %d %d %d", ledId, state, swtch, repeat));
+
+        return this;
+    }
+
+    @Override
+    public String toString()
+    {
+        StringBuilder result = new StringBuilder();
+
+        for (String command : commands)
+        {
+            result.append(command).append(" ");
+        }
+
+        return result.toString();
+    }
+}
